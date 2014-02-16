f8010e49655f649fa95c60c08723916d93c817d9
DeProdigy <alex.hint.ah@gmail.com>
2014-02-15 22:27:31 -0500

    rename welcom to search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Winow::Application.routes.draw do
-  root to: 'welcome#index'
-  get "welcome/index"
+  root to: 'search#index'
+  get "search/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
