ec7819631177e445161bd440f0008a992f785565
DeProdigy <alex.hint.ah@gmail.com>
2014-02-16 19:45:39 -0500

    uncomment previous input, rake assets precompile == true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Winow::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -79,5 +79,5 @@ Winow::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   #precompile all the assests in production
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb', '*.css.scss']
+  # config.assets.precompile = ['*.js', '*.css', '*.css.erb', '*.css.scss']
 end
