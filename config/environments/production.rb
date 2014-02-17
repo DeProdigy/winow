7abbf963385b3012236e2f1f6d45904cd9f4081c
DeProdigy <alex.hint.ah@gmail.com>
2014-02-16 19:43:13 -0500

    add config.assets.precompile =  [*.js, *.css, *.css.erb] to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Winow::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #precompile all the assests in production
+  config.assets.precompile = ['*.js', '*.css', '*.css.erb', '*.css.scss']
 end
